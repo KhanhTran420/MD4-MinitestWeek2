098a80f7b5c9bbfad9d44afa9a07bca639287e60
unknown <khanhbi1998pt@gmail.com>
2022-04-24 03:08:48 +0700

    khanh update 3:07 24/4/2022

---
+++ src/main/java/com/codegym/service/book/BookService.java
@@ -0,0 +1,41 @@
+package com.codegym.service.book;
+
+import com.codegym.model.Book;
+import com.codegym.model.Category;
+import com.codegym.repository.IBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class BookService implements IBookService{
+
+    @Autowired
+    private IBookRepository bookRepository;
+
+    @Override
+    public Iterable<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    @Override
+    public void save(Book book) {
+
+        bookRepository.save(book);
+    }
+
+    @Override
+    public void remove(Long id) {
+
+        bookRepository.deleteById(id);
+    }
+
+    @Override
+    public Iterable<Book> findAllByCategory(Category category) {
+        return bookRepository.findAllByCategory(category);
+    }
+}
